8f54d54ff1bdee81fb0e5172c980b04bb0f67593
jemustain <jemustain@gmail.com>
2026-03-20 19:05:19 -0700

    feat: add Hetzner Cloud provider, cloud-init generator, and provider abstraction
    - HetznerProvider: create/destroy/get/list/powerOn/powerOff via Hetzner Cloud API v1
    - CloudProvider interface for future DigitalOcean/Vultr support
    - Cloud-init template: Node.js, OpenClaw, sidecar, firewall, phone-home
    - Default: cx22 (2 vCPU/4GB) in nbg1 (Nuremberg)
    - Add HETZNER_API_TOKEN to .env.example
    
    Tasks: T10, T11

---
+++ apps/web/src/lib/providers/index.ts
@@ -0,0 +1,4 @@
+export { HetznerProvider } from "./hetzner";
+export { generateCloudInit } from "./cloud-init";
+export type { CloudProvider, ServerInfo, CreateServerOptions } from "./types";
+export type { CloudInitOptions } from "./cloud-init";
